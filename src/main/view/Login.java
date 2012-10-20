d3c7ca21db38199fbe3340a5c76000c7bfb5eef4
Viknes <Viknes@192.168.2.4>
2012-10-20 14:25:43 -0400

    Formatting and refactoring to make it more understandable

--- src/main/view/Login.java
+++ src/main/view/Login.java
@@ -20,75 +20,69 @@ import javax.swing.WindowConstants;
 
 import main.controller.GameMaker;
 
-public class Login extends JFrame
-{
+public class Login extends JFrame {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	// Variables declaration
-	private JLabel jLabel1;
-	private JLabel jLabel2;
-	private JTextField jTextField1;
-	private JPasswordField jPasswordField1;
-	private JButton jButton1;
+	private JLabel usernameLabel;
+	private JLabel passwordLabel;
+	private JTextField usernameText;
+	private JPasswordField passwordText;
+	private JButton loginButton;
 	private JPanel contentPane;
+
 	// End of variables declaration
-	
-	
+
 	public static void main(String[] args) {
 		new Login();
-    }
-	
-	public Login()
-	{
+	}
+
+	public Login() {
 		super();
 		create();
 		this.setVisible(true);
 	}
-	
-	private void create()
-	{
-		jLabel1 = new JLabel();
-		jLabel2 = new JLabel();
-		jTextField1 = new JTextField();
-		jPasswordField1 = new JPasswordField();
-		jButton1 = new JButton();
-		contentPane = (JPanel)this.getContentPane();
+
+	private void create() {
+		usernameLabel = new JLabel();
+		passwordLabel = new JLabel();
+		usernameText = new JTextField();
+		passwordText = new JPasswordField();
+		loginButton = new JButton();
+		contentPane = (JPanel) this.getContentPane();
 		//
 		// jLabel1
 		//
-		jLabel1.setHorizontalAlignment(SwingConstants.LEFT);
-		jLabel1.setForeground(new Color(0, 0, 255));
-		jLabel1.setText("username:");
+		usernameLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		usernameLabel.setForeground(new Color(0, 0, 255));
+		usernameLabel.setText("username:");
 		//
 		// jLabel2
 		//
-		jLabel2.setHorizontalAlignment(SwingConstants.LEFT);
-		jLabel2.setForeground(new Color(0, 0, 255));
-		jLabel2.setText("password:");
+		passwordLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		passwordLabel.setForeground(new Color(0, 0, 255));
+		passwordLabel.setText("password:");
 		//
 		// jTextField1
 		//
-		jTextField1.setForeground(new Color(0, 0, 255));
-		jTextField1.setSelectedTextColor(new Color(0, 0, 255));
-		jTextField1.setToolTipText("Enter your username");
-		jTextField1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e)
-			{
+		usernameText.setForeground(new Color(0, 0, 255));
+		usernameText.setSelectedTextColor(new Color(0, 0, 255));
+		usernameText.setToolTipText("Enter your username");
+		usernameText.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				jTextField1_actionPerformed(e);
 			}
-
 
 		});
 		//
 		// jPasswordField1
 		//
-		jPasswordField1.setForeground(new Color(0, 0, 255));
-		jPasswordField1.setToolTipText("Enter your password");
-		jPasswordField1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e)
-			{
+		passwordText.setForeground(new Color(0, 0, 255));
+		passwordText.setToolTipText("Enter your password");
+		passwordText.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				jPasswordField1_actionPerformed(e);
 			}
 
@@ -96,12 +90,11 @@ public class Login extends JFrame
 		//
 		// jButton1
 		//
-		jButton1.setBackground(new Color(204, 204, 204));
-		jButton1.setForeground(new Color(0, 0, 255));
-		jButton1.setText("Login");
-		jButton1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e)
-			{
+		loginButton.setBackground(new Color(204, 204, 204));
+		loginButton.setForeground(new Color(0, 0, 255));
+		loginButton.setText("Login");
+		loginButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				jButton1_actionPerformed(e);
 			}
 
@@ -112,11 +105,11 @@ public class Login extends JFrame
 		contentPane.setLayout(null);
 		contentPane.setBorder(BorderFactory.createEtchedBorder());
 		contentPane.setBackground(new Color(204, 204, 204));
-		addComponent(contentPane, jLabel1, 5,10,106,18);
-		addComponent(contentPane, jLabel2, 5,47,97,18);
-		addComponent(contentPane, jTextField1, 110,10,183,22);
-		addComponent(contentPane, jPasswordField1, 110,45,183,22);
-		addComponent(contentPane, jButton1, 150,75,83,28);
+		addComponent(contentPane, usernameLabel, 5, 10, 106, 18);
+		addComponent(contentPane, passwordLabel, 5, 47, 97, 18);
+		addComponent(contentPane, usernameText, 110, 10, 183, 22);
+		addComponent(contentPane, passwordText, 110, 45, 183, 22);
+		addComponent(contentPane, loginButton, 150, 75, 83, 28);
 		//
 		// login
 		//
@@ -126,62 +119,67 @@ public class Login extends JFrame
 		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		this.setResizable(false);
 	}
+
 	/** Add Component Without a Layout Manager (Absolute Positioning) */
-	private void addComponent(JPanel contentPane2,Component c,int x,int y,int width,int height)
-	{
-		c.setBounds(x,y,width,height);
+	private void addComponent(JPanel contentPane2, Component c, int x, int y,
+			int width, int height) {
+		c.setBounds(x, y, width, height);
 		contentPane2.add(c);
 	}
-	
-	private void jTextField1_actionPerformed(ActionEvent e)
-	{
-		
+
+	private void jTextField1_actionPerformed(ActionEvent e) {
+
 	}
-	private void jPasswordField1_actionPerformed(ActionEvent e)
-	{
-		
+
+	private void jPasswordField1_actionPerformed(ActionEvent e) {
+
 	}
-	private void jButton1_actionPerformed(ActionEvent e)
-	{
-		String username = new String(jTextField1.getText());
-		String password = new String(jPasswordField1.getPassword());
-		
-		if(username.equals("") || password.equals("")) // If password and username is empty > Do this >>>
-				{
-					jButton1.setEnabled(false);
-				JLabel errorFields = new JLabel("<HTML><FONT COLOR = Blue>You must enter a username and password to login.</FONT></HTML>");	
-				JOptionPane.showMessageDialog(null,errorFields); 
-				jTextField1.setText("");
-	   		    jPasswordField1.setText("");
-	   		    jButton1.setEnabled(true);
-				this.setVisible(true);
-				}
-				else
-				{
-		JLabel optionLabel = new JLabel("<HTML><FONT COLOR = Blue>You entered</FONT><FONT COLOR = RED> <B>"+username+"</B></FONT> <FONT COLOR = Blue>as your username.<BR> Is this correct?</FONT></HTML>");
-int confirm =JOptionPane.showConfirmDialog(null,optionLabel);
-switch(confirm){       // Switch > Case
-	   	case JOptionPane.YES_OPTION:  // Attempt to Login user
-	   	jButton1.setEnabled(false);   // Set button enable to false to prevent 2 login attempts
-	   	break;
-            case JOptionPane.NO_OPTION:   // No Case.(Go back. Set text to 0)
-	   		jButton1.setEnabled(false);
-	   		jTextField1.setText("");
-	   		jPasswordField1.setText("");
-	   		jButton1.setEnabled(true);
-            break;
-	   		
-	   		case JOptionPane.CANCEL_OPTION:  // Cancel Case.(Go back. Set text to 0)
-	   	    jButton1.setEnabled(false);
-	   	    jTextField1.setText("");
-	   	    jPasswordField1.setText("");
-	   	    jButton1.setEnabled(true);
-	   	    break;
-	   	
-	   	}   // End Switch > Case
-	   	   
-	  
-	   }
+
+	private void jButton1_actionPerformed(ActionEvent e) {
+		String username = new String(usernameText.getText());
+		String password = new String(passwordText.getPassword());
+
+		if (username.equals("") || password.equals("")) // If password and
+														// username is empty >
+														// Do this >>>
+		{
+			loginButton.setEnabled(false);
+			JLabel errorFields = new JLabel(
+					"You must enter a username and password to login");
+			JOptionPane.showMessageDialog(null, errorFields);
+			usernameText.setText("");
+			passwordText.setText("");
+			loginButton.setEnabled(true);
+			this.setVisible(true);
+		} else {
+			JLabel optionLabel = new JLabel(
+					"<HTML>You entered"
+							+ username
+							+ "as your username.<BR> Is this correct?</HTML>");
+			int confirm = JOptionPane.showConfirmDialog(null, optionLabel);
+			switch (confirm) { // Switch > Case
+			case JOptionPane.YES_OPTION: // Attempt to Login user
+				loginButton.setEnabled(false); // Set button enable to false to
+											// prevent 2 login attempts
+				break;
+			case JOptionPane.NO_OPTION: // No Case.(Go back. Set text to 0)
+				loginButton.setEnabled(false);
+				usernameText.setText("");
+				passwordText.setText("");
+				loginButton.setEnabled(true);
+				break;
+
+			case JOptionPane.CANCEL_OPTION: // Cancel Case.(Go back. Set text to
+											// 0)
+				loginButton.setEnabled(false);
+				usernameText.setText("");
+				passwordText.setText("");
+				loginButton.setEnabled(true);
+				break;
+
+			} // End Switch > Case
+
+		}
 		new GameMaker();
 	}
 }
